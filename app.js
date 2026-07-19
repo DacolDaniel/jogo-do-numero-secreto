a100281c3befcc74b9d1e5a7e83c7a319e2376ce
daniel <daniel@jettax.com.br>
2026-07-19 14:16:56 -0300

    [dacol]-projeto inicial

---
+++ app.js
@@ -0,0 +1,78 @@
+
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O numero secreto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O numero secreto é maior!');
+        }
+        tentativas ++;        
+        limparCampo();
+    }
+}
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    //responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'jogo do numero secreto');
+    exibirTextoNaTela('p', `Escolha um numero entre 1 e ${numeroLimite}`);    
+    mostrarDicas();
+}
+
+exibirMensagemInicial();
+
+function gerarNumeroAleatorio() {    
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function mostrarDicas() {
+    let dicas = [];
+
+    dicas.push(`O número é ${numeroSecreto % 2 === 0 ? 'par' : 'ímpar'}.`);
+    dicas.push(`Se multiplicar por 3 e dividir por 2, o resultado é ${(numeroSecreto * 3) / 2}.`);
+
+    document.getElementById('listaDicas').innerHTML =
+        dicas.map(dica => `<li>${dica}</li>`).join('');
+}
